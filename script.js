117f3edcd92d3586266f7757b0047db17c5e3838
Rupesh Sagaram <s.v.rupesh97@gmail.com>
2024-08-24 14:35:08 +0530

    added a operate function and basic html elements for the buttons and display

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ function add(...values){
     let arr =[];
     arr.push(...values);
     let result = arr.reduce((total,current)=> total + current);
+        // console.log(result);
         return result;
 }
 
@@ -27,4 +28,33 @@ function divide(...values){
     let result = arr.reduce((total,current)=> total / current);
        return result;
 }
+
+//variables for calculator operation , two numbers and one operator
+
+let number1;
+let operator;
+let number2;
+
+function operate(number1, operator, number2){
+    switch(operator){
+        case add: 
+            add(number1, number2);
+        break;
+
+        case subtract:
+            subtract(number1,number2);
+        break;
+
+        case multiply:
+            multiply(number1,number2);
+        break;
+
+        case divide:
+            divide(number1,number2);
+        break;  
+    }
+}
+
+
+
 
